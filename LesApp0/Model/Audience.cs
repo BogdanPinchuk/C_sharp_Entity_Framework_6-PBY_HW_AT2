4c38c7b1aecf02a466418fe2fe4cbfce7e084d19
Bogdan <pinchuk.brus@windowslive.com>
2019-10-25 16:11:28 +0300

    Add LesApp0

---
+++ LesApp0/Model/Audience.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp0.Model
+{
+    /// <summary>
+    /// Аудиторія
+    /// </summary>
+    public class Audience
+    {
+    }
+}
---
+++ LesApp0/Model/Trainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp0.Model
+{
+    /// <summary>
+    /// Тренер
+    /// </summary>
+    public class Trainer
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// ПІБ
+        /// </summary>
+        public string FullName { get; set; }
+
+    }
+}
